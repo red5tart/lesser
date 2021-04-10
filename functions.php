1b78e2b6da4f271e2acd2502fede9bf85a3351dd
red5tart <red5tart@mail.ru>
2021-04-10 12:16:21 +0300

    4post block on homepage

--- functions.php
+++ functions.php
@@ -2,8 +2,13 @@
 //Добавление расширенных возможностей
 if ( ! function_exists( 'lesser_theme_setup' ) ) :
   function lesser_theme_setup() {
+
     // Добавление тега title 
 		add_theme_support( 'title-tag' );
+
+		//Добавление миниатюр
+		add_theme_support( 'post-thumbnails', array( 'post' ) );// Только для post
+		
 		// Добавление пользовательского логотипа
 		add_theme_support( 'custom-logo', [
 			'width'       => 27,
@@ -27,7 +32,7 @@ function enqueue_lesser_style() {
   wp_enqueue_style( 'style', get_stylesheet_uri() );
 	wp_enqueue_style( 'lesser-theme-style', get_template_directory_uri() . '/assets/css/lesser-theme.css', 'style', time());
 	wp_enqueue_style( 'Playfair-Display', 'https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap');
-	wp_enqueue_style( 'Roboto', 'https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+	wp_enqueue_style( 'Roboto', 'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap');
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_lesser_style' );
 
@@ -36,7 +41,6 @@ add_filter( 'intermediate_image_sizes', 'delete_intermediate_image_sizes' );
 function delete_intermediate_image_sizes( $sizes ){
 	// размеры которые нужно удалить
 	return array_diff( $sizes, [
-		'medium_large',
 		'large',
 		'1536x1536',
 		'2048x2048',
--- home.php
+++ home.php
@@ -1,5 +1,82 @@
 <?php get_header( );?>
 
-<h1>Привет, это home.php!</h1>
+<main>
+  <div class="container">
+    <div class="hero">
+      <h1 class="hero-title">Курс на социально-ориентированный национальный проект связывает нас с нашим прошлым</h1>
 
-<?get_footer( );?>
+      <!-- начало мозаики 4х постов -->
+      <ul class="article-grid">
+        <?php		
+        global $post;
+        //формируем запрос в базу данных
+        $query = new WP_Query( [
+          //получаем 4 поста
+          'posts_per_page' => 4,
+          'category__not_in' => 32,
+          ] );
+        //проверяем,есть ли посты
+        if ( $query->have_posts() ) {
+          //создаем переменную-счетчик постов
+          $cnt = 0;
+          //пока посты есть, выводим их
+          while ( $query->have_posts() ) {
+            $query->the_post();
+            //и увеличиваем счетчик постов
+            $cnt++;
+            switch ($cnt) {
+              //выводим первый пост
+              case '1':
+                ?>
+                  <li class="article-grid-item article-grid-item-1">
+                    <a href="<?php echo the_permalink()?>" class="article-grid-permalink">
+                      <span class="category-name"><?php $category = get_the_category(); echo $category[0]->name;?></span>
+                      <h4 class="article-grid-title"><?php echo mb_strimwidth(get_the_title(), 0, 60, '...');
+                      ?></h4>
+                    </a>
+                  </li>
+                <?php
+                break;
+
+              //выводим второй пост
+              case '2':
+                ?>
+                  <li class="article-grid-item article-grid-item-2">
+                    <a href="<?php echo the_permalink()?>" class="article-grid-permalink">
+                      <span class="category-name"><?php $category = get_the_category(); echo $category[0]->name;?></span>
+                      <h4 class="article-grid-title"><?php echo mb_strimwidth(get_the_title(), 0, 60, '...');
+                      ?></h4>
+                    </a>
+                  </li>
+                <?php
+                break;
+
+              //выводим остальные посты
+              default:
+                ?>
+                  <li class="article-grid-item article-grid-item-default">
+                    <a href="<?php echo the_permalink()?>" class="article-grid-permalink">
+                      <span class="category-name"><?php $category = get_the_category(); echo $category[0]->name;?></span>
+                      <h4 class="article-grid-title"><?php echo mb_strimwidth(get_the_title(), 0, 40, '...');?></h4>
+                    </a>
+                  </li>
+                <?php 
+                break;
+            }
+            ?>
+            <!-- Вывода постов, функции цикла: the_title() и т.д. -->
+            <?php 
+          }
+        } else {
+          // Постов не найдено
+        }
+
+        wp_reset_postdata(); // Сбрасываем $post
+        ?>
+      </ul>
+      <!-- /.article-grid -->
+    </div><!-- /.hero -->
+  </div>
+  <!-- /.container -->
+</main>
+<?php get_footer(); ?>
