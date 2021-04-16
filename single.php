7d9d5c07f4ce5899b52bca5f87e4108ecfcaf64e
red5tart <red5tart@mail.ru>
2021-04-16 11:29:38 +0300

    add single page

--- single.php
+++ single.php
@@ -1,32 +1,16 @@
 <?php get_header('post');?>
+  <main class="site-main">
+    <?php
+    //запускаем цикл WordPress и проверяем наличие постов
+    //если пост есть
+		while ( have_posts() ) :
+      //выводим все его содержимое
+			the_post();
 
-<div class="container">
-<div class="left">
-  <?php
-  // запускаем цикл wordpress, проверяем, есть ли посты
-  while ( have_posts() ) :
-    // если пост есть, выводим содержимое
-    the_post( );
-    // находим шаблон для вывода поста в папке template_parts
-    get_template_part( 'template-parts/content', get_post_type() );
+      //находим шаблон для вывода поста в папке template-parts, файл content.php
+			get_template_part( 'template-parts/content', get_post_type() );
 
-  endwhile; // Конец цикла WP
-  ?>
-</div>
-<div class="right">
-<?php 
-  wp_nav_menu( [
-          'theme_location'  => 'post-side_menu',
-          'container'       => 'nav', 
-          'container_class' => 'post-side-nav', 
-          'menu_class'      => 'post-side-menu', 
-          'echo'            => true,
-        ] );
-?>
-</div>
-
-</div>
-
-<?php
-get_sidebar();
-get_footer( );?>
+		endwhile; // Конец цикла while Wordpress
+		?>
+  </main>
+<?php get_footer();?>
--- template-parts/content.php
+++ template-parts/content.php
@@ -1,61 +1,55 @@
+<!--конкретный пост определяется по ID-->
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<!-- шапка поста -->
-	<header class="entry-header <?php echo get_post_type();?>-header" style="background: linear-gradient(0deg, rgba(38, 45, 51, 0.75), rgba(38, 45, 51, 0.75)), url(
-    <?php 
-      if( has_post_thumbnail() ) {
-          echo get_the_post_thumbnail_url();
-        }
-        else {
-          echo get_template_directory_uri().'/assets/images/img-default.png';
-        }
-      ?>
-  );">
-    <div class="container">
-      <div class="post-header-wrapper">
-        <div class="post-title-wrapper">
-          <?php
-          // проверяем, точно ли мы на странице поста
-          if ( is_singular() ) :
-            the_title( '<h1 class="post-title">', '</h1>' );
-          else :
-            the_title( '<h2 class="post-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-          endif;?>
-        </div>
-        <!-- /.post-title-wrapper -->
-      </div>
-      <!-- /.post-header-wrapper -->
-      </div>
-    <!-- /.container -->
-	</header><!-- .entry-header -->
-
+  <!--выводим картинку, контент поста, сайдбар и комменты для конкретного поста-->
+  <!--инфа в пределах контейнера-->
   <div class="container">
-    <!-- Содержимое поста -->
-    <div class="post-content">
-      <?php
-      // выводим содержимое
-      the_content(
-        sprintf(
-          wp_kses(
-            /* translators: %s: Name of current post. Only visible to screen readers */
-            __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'uni-example' ),
-            array(
-              'span' => array(
-                'class' => array(),
-              ),
-            )
-          ),
-          wp_kses_post( get_the_title() )
-        )
-      );
+    <?php  //вывод заголовка
+      //если мы на стрнице поста
+      if ( is_singular() ) : {   //то...
+        //выводим заголовок в теге h1
+        ?>
+        
+          <h1 class="post-title"> <?php the_title();?></h1>
+        <?php
+      }
+      else : {
+        //иначе - заголовок в теге h2 и ссылку на пост
+        the_title( '<h2 class="post-title"><a href="' . esc_url( get_permalink() ) . '"    rel="bookmark">', '</a></h2>' );
+      }
+      endif; 
+    ?>
+  <div class="row">
+    <div class="post-wrapper-content">
+      <div class="post-picture">
+        <img src="<?php 
+          if ( has_post_thumbnail() ) {
+              echo get_the_post_thumbnail_url();
+          }
+          else {
+              echo get_template_directory_uri().'/assets/images/img-default.png';
+          }
+          ?>"class="post-picture-thumb"> 
+      </div><!--/.post-picture-->
+      <div class="post-content">
+        <?php the_content();?>
+      </div> <!-- /.post-content -->
+    </div><!--/.post-wrapper-content-->
 
-      wp_link_pages(
-        array(
-          'before' => '<div class="page-links">' . esc_html__( 'Страницы:', 'uni-theme' ),
-          'after'  => '</div>',
-        )
-      );
-      ?>
-    </div><!-- .post-content -->
-    <!-- Подвал поста -->
-  </div><!-- /.container -->
-</article> 
+    <div class="post-wrapper-menu">
+
+      <div class="post-wrapper-sideline">
+        <h2>Категории</h2>
+        <?php 
+          wp_nav_menu( [
+                  'theme_location'  => 'post-side_menu',
+                  'container'       => 'nav', 
+                  'container_class' => 'post-side-nav', 
+                  'menu_class'      => 'post-side-menu', 
+                  'echo'            => true,
+                ] );
+        ?>
+      </div>
+    </div> <!-- /.post-wrapper-menu -->
+  </div><!--/.row-->
+  </div><!--/.container-->
+</article>
